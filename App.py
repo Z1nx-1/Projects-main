b31cca2b0fd0caf1ba440caab60b3dcb9a31ddf9
Jack <86924197+Z1nx-1@users.noreply.github.com>
2023-10-01 19:05:51 +0100

    "Reverted back to old method(without use of the class), Fixed adult variable."

--- App.py
+++ App.py
@@ -1,16 +1,12 @@
-from UserClass import userclass
-
-
 def credentials():
+    global name, age, nationality, adult
     name = input("Please enter your name: ")
     age = int(input("Please enter your age: "))
     nationality = input("Please enter your nationality: ")
-    adult = True
     if age >= 18:
-        print("Adult")
-    elif age <= 18:
-            adult = False
-            print("Non-Adult")
+        adult = "Adult"
+    if age <= 18:
+        adult = "Non adult"
     print(name, age, nationality, adult)
     incorrect = input("Is this correct? Y/N: ")
     if incorrect == "Y":
@@ -21,10 +17,28 @@ def credentials():
             name = input("Please enter your name: ")
             age = int(input("Please enter your age: "))
             nationality = input("Please enter your nationality: ")
+            if age >= 18:
+                adult = "Adult"
+            if age <= 18:
+                adult = "Non adult"
             print(name, age, nationality, adult)
             true = input("Is this correct? Y/N: ")
             if true == 'Y':
                 correct = True
 
 
+def data_processing():
+    data_processing = open("user_data.txt", "w")
+    data_processing.write(name)
+    data_processing.write("\n")
+    data_processing.write(str(age))
+    data_processing.write("\n")
+    data_processing.write(nationality)
+    data_processing.write("\n")
+    data_processing.write(adult)
+    data_processing.write("\n")
+    data_processing.close()
+
+
 credentials()
+data_processing()
