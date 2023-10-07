cca8432874a9cd737e21498e75945f24a0aa257a
Jack <86924197+Z1nx-1@users.noreply.github.com>
2023-10-07 15:24:15 +0100

    ""

--- Advanced_Cal.py
+++ Advanced_Cal.py
@@ -1,72 +1,63 @@
-def cal1():
-    while True:
-        try:
-            number1 = float(input("Enter first number: "))
-        except ValueError:
-            print("Invalid, Repeat: ")
-            continue
-        print("(+, -, /, *)")
-        try:
-            operator = input("Enter operator: ")
-        except ValueError:
-            print("Invalid, Repeat: ")
-            continue
-        try:
-            number2 = float(input("Enter second number: "))
-        except ValueError:
-            print("Invalid, Repeat: ")
-            continue
-        else:
-            break
-    if operator == "+":
-        result = float(number1 + number2)
-    elif operator == "-":
-        result= float(number1 - number2)
-    elif operator == "/":
-        result = float(number1 / number2)
-    elif operator == "*":
-        result = float(number1 * number2)
-    print(result)
-    return result
+while True:
+    def calculator():
+        while True:
+            while True:
+                try:
+                    number1 = float(input("Enter first number: "))
+                except ValueError:
+                    print("Invalid, Repeat: ")
+                    continue
+                print("(+, -, /, *)")
+                try:
+                    operator = input("Enter operator: ")
+                except ValueError:
+                    print("Invalid, Repeat: ")
+                    continue
+                try:
+                    number2 = float(input("Enter second number: "))
+                except ValueError:
+                    print("Invalid, Repeat: ")
+                    continue
+                if operator == "+":
+                    result = float(number1 + number2)
+                elif operator == "-":
+                    result= float(number1 - number2)
+                elif operator == "/":
+                    result = float(number1 / number2)
+                elif operator == "*":
+                    result = float(number1 * number2)
+                print(result)
+                return result
 
 
-def cal2(original):
-    cont = input("Would you like to continue? Y/N: ")
-    if cont == 'Y':
-        print("continuing: ")
-    if cont == 'N':
-        print("Quiting: ")
-        quit()
-    while True:
-        try:
-            number2 = original
-            number3 = float(input("Enter first number: "))
-        except ValueError:
-            print("Invalid, repeat: ")
-        try:
+    def calculator_loop(original):
+        while True:
+            cont = input("Add another number? Y/N: ")
+            if cont == 'Y':
+                print("continuing: ")
+            if cont == 'N':
+                quit()
             print("(+, -, /, *)")
-            operator = input("Enter operator: ")
+            try:
+                operator = input("Enter operator: ")
+            except ValueError:
+                print("Invalid, Repeat: ")
+                continue
+            try:
+                number3 = float(input("Enter first number: "))
+            except ValueError:
+                print("Invalid, Repeat: ")
+                continue
             if operator == "+":
-                result2 = float(number2 + number3)
+                result2 = float(original + number3)
             elif operator == "-":
-                result2 = float(number2 - number3)
+                result2 = float(original - number3)
             elif operator == "/":
-                result2 = float(number2 / number3)
+                result2 = float(original / number3)
             elif operator == "*":
-                result2 = float(number2 * number3)
+                result2 = float(original * number3)
             print(result2)
-        except:
-            print("Invalid operator")
-        else:
-            break
 
 
-def end():
-    k = input("Enter k to quit: ")
-    if k == 'k':
-        quit()
-
-
-result = cal1()
-cal2(result)
-end()
+    original = calculator()
+    calculator_loop(original)
